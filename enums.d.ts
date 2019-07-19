f4aa81983a75704ec41650ef8d0a37e4ca050925
Stephen-kongxiangwei <53067722+Stephen-kongxiangwei@users.noreply.github.com>
2019-07-19 14:24:36 +0800

    Update enums.d.ts

--- enums.d.ts
+++ enums.d.ts
@@ -25,27 +25,47 @@
 
 
     declare enum RemoteButton {   
-      CH_MINUS = 0x45,
-	  CH = 0x46,
-	  CH_Add = 0x47,
-	  PREV = 0x44,
-	  PLAY = 0x43,
-	  NUM_200 = 0xd,
-	  NEXT = 0x40,
-	  NUM_100 = 0x19,
-	  Minus = 0x7,
-	  Add = 0x15,
-	  EQ = 0x9,
-	  NUM0 = 0x16,
-	  NUM1 = 0xc,
-	  NUM2 = 0x18,
-	  NUM3 = 0x5e,
-	  NUM4 = 0x8,
-	  NUM5 = 0x1c,
-	  NUM6 = 0x5a,
-	  NUM7 = 0x42,
-	  NUM8 = 0x52,
-	  NUM9 = 0x4A,
+      CH_MINUS = 0x4A,//保留
+	  
+CH = 0xc,//保留
+	 
+ CH_Add = 0x5e,//保留
+	  
+PREV = 0x18,//上
+	  
+PLAY = 0x1c,//OK
+	  
+NUM_200 = 0xd,//
+	 
+ NEXT = 0x52,//下
+	  
+NUM_100 = 0x16,//
+	  
+Minus = 0x8,//左
+	  
+Add = 0x5a,//右
+	  
+EQ = 0x42,//保留
+	  
+NUM0 = 0x19,
+	  
+NUM1 = 0x45,
+	  
+NUM2 = 0x46,
+	  
+NUM3 = 0x47,
+	  
+NUM4 = 0x44,
+	  
+NUM5 = 0x40,
+	 
+ NUM6 = 0x43,
+	  
+NUM7 = 0x7,
+	  
+NUM8 = 0x15,
+	  
+NUM9 = 0x9,
     }
 declare namespace Mbit_IR {
 }
